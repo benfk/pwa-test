0b6bc49aff01771915fb580ceaf66c23908a1d8a
benkissi <bkf@jetstreamafrica.com>
2021-11-16 14:59:26 +0000

    feat(); add A2HS button

---
+++ src/components/A2HSBtn.vue
@@ -0,0 +1,42 @@
+<template>
+  <button v-if="showA2HSBtn" class="action" @click="handleClick">
+    Add Jetstream to your phone
+  </button>
+</template>
+
+<script>
+export default {
+  name: "A2HSBtn",
+  data() {
+    return {
+      deferredPrompt: null,
+    };
+  },
+  created() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+    });
+  },
+  computed: {
+    showA2HSBtn() {
+      return this.deferredPrompt;
+    },
+  },
+  methods: {
+    handleClick() {
+      console.log("clicked");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.action {
+  padding: 10px;
+  background: #42b983;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <A2HS/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import A2HS from "@/components/A2HSBtn"
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    A2HS
   },
 };
 </script>
